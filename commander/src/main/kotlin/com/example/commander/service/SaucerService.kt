13fefbe11026d8dde5631935c6c0de074f422d61
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2023-07-18 23:25:01 -0500

    Update SaucerView

--- commander/src/main/kotlin/com/example/commander/service/SaucerService.kt
+++ commander/src/main/kotlin/com/example/commander/service/SaucerService.kt
@@ -1,7 +1,10 @@
 package com.example.commander.service
 
+import com.example.commander.model.DetailView
 import com.example.commander.model.Saucer
+import com.example.commander.model.SaucerView
 import com.example.commander.repository.SaucerRepository
+import com.example.commander.repository.SaucerRepositoryView
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -13,8 +16,15 @@ class SaucerService {
     @Autowired
     lateinit var saucerRepository: SaucerRepository
 
+    @Autowired
+    lateinit var saucerRepositoryView: SaucerRepositoryView
+
     fun list(): List<Saucer> {
         return saucerRepository.findAll()
+    }
+
+    fun listProteinsSaucer (): List<SaucerView>{
+        return saucerRepositoryView.findAll()
     }
 
     fun save(saucer: Saucer):  Saucer? {
